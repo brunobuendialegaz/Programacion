ef3f049744a8bb3d37e28c9717ec8d05e7e61a34
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-10-13 17:09:16 +0200

    update

---
+++ Ejercicio 6/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 16;
+        int numero2 = 2;
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int division = numero1 / numero2;
+        int multiplicacion = numero1 * numero2;
+
+        System.out.println("Numero 1: "+numero1);
+        System.out.println("Numero 2: "+numero2);
+        System.out.println("Suma: "+suma);
+        System.out.println("Resta: "+resta);
+        System.out.println("Multiplicación: "+multiplicacion);
+        System.out.println("División: "+division);
+    }
+}
