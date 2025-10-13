dd4b2b52b87f380d5ac6bddc12542d1714c82c45
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-10-13 17:16:09 +0200

    update

---
+++ Ejercicio 7/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Bruno Buendia";
+        int edad = 30;
+        String ciudad = "Librilla";
+        System.out.println("Hola, me llamo "+nombre+".");
+        System.out.println("Tengo "+edad+" años y vivo en "+ciudad+".");
+    }
+}
