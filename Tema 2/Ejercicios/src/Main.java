be68321899196111ad188eb169e2abe691696545
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-10-19 13:04:55 +0200

    update

--- Tema 2/Ejercicios/src/Main.java
+++ Tema 2/Ejercicios/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Metodo metodo = new Metodo();
         //metodo.ejercicio1();
-        
+        //metodo.ejercicio2();
     }
 }
--- Tema 2/Ejercicios/src/Metodos/Metodo.java
+++ Tema 2/Ejercicios/src/Metodos/Metodo.java
@@ -26,8 +26,33 @@ public class Metodo {
                 resto);
         scanner.close();
     }
+    public void ejercicio2(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el primer número:");
+        int op1 = scanner.nextInt();
+        System.out.println("Introduce el segundo número:");
+        int op2 = scanner.nextInt();
+        boolean mayor = op1>op2;
+        boolean menor = op1<op2;
+        boolean igual = op1==op2;
+        boolean diferente = op1!=op2;
+        boolean mayorIgual = op1>=op2;
+        boolean menorIgual = op1<=op2;
+        System.out.printf("¿%d es mayor que %d?: %b%n" + //puedo concatenar texto con +, pero despues deben ir todas las variables seguidas con comas
+                "¿%d es menor que %d?: %b%n"+
+                "¿%d es igual a %d?: %b%n"+
+                "¿%d es diferente a %d?: %b%n"+
+                "¿%d es mayor o igual a %d? %b%n"+
+                "¿%d es menor o igual a %d?: %b",
+                op1,op2,mayor,
+                op1,op2,menor,
+                op1,op2,igual,
+                op1,op2,diferente,
+                op1,op2,mayorIgual,
+                op1,op2,menorIgual);
+        scanner.close();
+    }
+    public void ejercicio3(){
 
-
-
-
+    }
 }
