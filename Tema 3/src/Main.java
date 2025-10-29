f387967d29969cb450a4b664619f92f667630737
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-10-29 13:43:12 +0100

    EjT3
    Todos completos

--- Tema 3/src/Main.java
+++ Tema 3/src/Main.java
@@ -13,7 +13,12 @@ public class Main {
         //t3.ej7();
         //t3.ej8();
         //t3.ej9();
-        t3.ej15();
+        //t3.ej10();
+        //t3.ej11();
+        //t3.ej12();
+        //t3.ej13();
+        //t3.ej14();
+        //t3.ej15();
         t3.scannerClose();
     }
 }
--- Tema 3/src/ejerciciosT3/T3.java
+++ Tema 3/src/ejerciciosT3/T3.java
@@ -1,5 +1,8 @@
 package ejerciciosT3;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class T3 {
@@ -114,6 +117,136 @@ public class T3 {
             System.out.println("Estas en un peso sano segun tu IMC.");
         }
 
+    }
+    public void ej10(){
+        System.out.print("Introduce tu altura en cm: ");
+        int altura = s.nextInt();
+        if (altura>=120&&altura<=200){
+            System.out.println("Puedes subirte, la altura minima es 120");
+        } else if (altura<120) {
+            System.out.println("No puedes subirte, la latura mínima es 120");
+        }else
+        {
+            System.out.println("No puedes subirte, la altura máxima es 200");
+        }
+    }
+    public void ej11(){
+        System.out.print("Introduce el importe de la compra: ");
+        double valor1 = s.nextDouble();
+        System.out.println("¿Eres socio (true/false)?: ");
+        boolean socio = s.nextBoolean();
+        s.nextLine();
+        System.out.println("Importe original: "+valor1);
+        double valor2 = 0;
+        if (valor1>=200&&socio){
+            valor2=valor1*0.2;
+            System.out.println("Eres socio y tu compra es >= 200€");
+            System.out.printf("Descuento aplicado (20%%): %.2f%n",valor2);
+        } else if (valor2<200&&socio) {
+            valor2=valor1*0.1;
+            System.out.println("Eres socio y tu compra es <200");
+            System.out.printf("Descuento aplciado (10%%): %.2f%n",valor2);
+        } else if (valor2>=300&&!socio) {
+            valor2=valor1*0.05;
+            System.out.println("No eres socio pero tu compra es >=300");
+            System.out.printf("Descuento aplicado (5%%): %.2f%n",valor2);
+        }else {
+            System.out.println("No cumples ningun requisito, no se aplica descuento.");
+        }
+        System.out.printf("Importe final: %.2f€",valor1-valor2);
+    }
+    public void ej12(){
+        System.out.print("introduce una contraseña: ");
+        String contrasena = s.nextLine();
+        System.out.println("Longitud decontraseña: "+contrasena.length());
+        boolean longitud = contrasena.length()>=8;
+        System.out.println("¿Tiene al menos 8 caracteres? "+longitud);
+        boolean numero = contrasena.matches(".*[0-9].*");
+        System.out.println("¿La contraseña tiene al menos 1 número? "+numero);
+        //boolean prohibida = contrasena.contains("12345678")||contrasena.contains("password");
+        boolean prohibida = contrasena.equals("12345678")||contrasena.equals("password");
+        System.out.println("¿Es una contraseña prohibida? "+prohibida);
+        boolean valida = longitud&&numero&&!prohibida;
+        System.out.println("¿Es una contraseña valida? "+valida);
+        if (valida){
+            System.out.println("Es una contraseña valida. ");
+        } else {
+            System.out.print("La contraseña no es valida. ");
+            if (!longitud){
+                System.out.print("Debe tener al menos 8 caracteres. ");
+            }
+            if (!numero){
+                System.out.print("Debe contener 1 número.");
+            }
+            if (prohibida){
+                System.out.print("Debe ser distinta de 12345678 y password.");
+            }
+        }
+    }
+    public void ej13(){
+        System.out.print("Introduce tu edad: ");
+        int edad = s.nextInt();
+        System.out.print("introduce dia de la semana (1-7): ");
+        int dia = s.nextInt();
+        System.out.println("Edad: "+edad+" años");
+        if (dia>0&&dia<8){
+            DayOfWeek diaNombre = DayOfWeek.of(dia);
+            TextStyle estilo = TextStyle.FULL;
+            Locale espanol = new Locale("es","ES");
+            String nombreDiaEspañol = diaNombre.getDisplayName(estilo, espanol);
+            System.out.println("Dia de la semana: "+dia+" "+nombreDiaEspañol);
+            double precio;
+            if (edad<12){
+                precio=0;
+                System.out.printf("Precio de entrada: %.2f€. Entrada gratis para menores de 12 años",precio);
+            } else if (edad>12&&edad<18&&dia==2) {
+                precio=0;
+                System.out.printf("Precio de entrada: %.2f€. Entrada gratis para menores los martes",precio);
+            }else if (edad>12&&edad<18&&dia!=2) {
+                precio=5;
+                System.out.printf("Precio de entrada: %.2f€.",precio);
+            }else if (edad>18&&edad<64&&dia==4) {
+                precio=7;
+                System.out.printf("Precio de entrada: %.2f€. Precio reducido los jueves",precio);
+            }else if (edad>18&&edad<64&&dia!=4) {
+                precio=10;
+                System.out.printf("Precio de entrada: %.2f€.",precio);
+            }else if (edad>65) {
+                precio=6;
+                System.out.printf("Precio de entrada: %.2f€.",precio);
+            }
+
+        }else {
+            System.out.println("El dia seleccionado no es valido.");
+        }
+
+    }
+    public void ej14(){
+        System.out.print("Introduce tu edad: ");
+        int edad = s.nextInt();
+        System.out.print("Introduce tus ingresos mensuales: ");
+        double ingresos = s.nextDouble();
+        System.out.print("¿Tienes deudas pendientes? (true/false): ");
+        boolean deudas = s.nextBoolean();
+        boolean edadApto = edad>=21&&edad<=65;
+        System.out.println("¿Edad entre 21 y 65?: "+edadApto);
+        boolean ingresosApto = ingresos>=1000;
+        System.out.println("¿Ingresos >= 1000€?: "+ingresosApto);
+        System.out.println("¿Sin deudas pendientes?: "+!deudas);
+        if (edadApto&&ingresosApto&&!deudas){
+            System.out.println("Felicidades, eres apto para el prestamo.");
+        }else {
+            System.out.print("Lastima, no eres apto. ");
+            if (!edadApto){
+                System.out.print("No estas dentro del rango de edad. ");
+            }
+            if (!ingresosApto){
+                System.out.print("No tienes los ingresos minimos. ");
+            }
+            if (deudas){
+                System.out.println("Tienes deudas pendientes con otras entidades. ");
+            }
+        }
     }
     public void ej15(){
         System.out.print("Introduce el peso del paquete (kg): ");
