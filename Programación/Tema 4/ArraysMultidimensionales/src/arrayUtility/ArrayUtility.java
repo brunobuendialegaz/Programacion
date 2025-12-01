76f793f36c6db0b660574f6997fc0c844717308c
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-12-01 11:13:10 +0100

    ej arraybid 1-5

---
+++ Programación/Tema 4/ArraysMultidimensionales/src/arrayUtility/ArrayUtility.java
@@ -0,0 +1,91 @@
+package arrayUtility;
+
+import java.util.Scanner;
+
+public class ArrayUtility {
+
+    Scanner s = new Scanner(System.in);
+
+    public void crearArrayInt( int[][] array, int rango){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random()*rango);
+            }
+        }
+    }
+
+    public void crearArrayDouble( double[][] array, int rango){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = Math.random()*rango;
+            }
+        }
+    }
+
+    public void recorrerArrayBidimensional(int[][] array){
+        for(int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]+" ");
+                if (j==array[i].length-1){
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
+    public void recorrerArrayBidDouble(double[][] array){
+        for(int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%.2f ",array[i][j]);
+                if (j==array[i].length-1){
+                    System.out.print("\n");
+                }
+            }
+        }
+    }
+
+    public void rellenarArrayBidScanner(int[][] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%nIntroduce el numero de la posición [%d][%d]: ",i,j);
+                array[i][j] = s.nextInt();
+            }
+        }
+    }
+
+    public int sumaArrayBidimensional (int[][] array, int total){
+
+        for(int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++) {
+                total += array[i][j];
+                }
+            }
+        return total;
+    }
+
+    public String buscarEnArrayBid (int[][] array, int numObjetivo){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (numObjetivo==array[i][j]){
+                    return "El número "+numObjetivo+" se encuentra en la posición ["+i+"]["+j+"]";
+                }
+            }
+        }
+        return "El número "+numObjetivo+" no se encuentra en la matriz";
+    }
+
+    public void mediaFila(double[][] array){
+        double total = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                total+=array[i][j];
+                if (j==array[i].length-1){
+                    System.out.printf("La media de la fila %d es %.2f%n",i,total/array[i].length);
+                    total=0;
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Programación/Tema 4/ArraysMultidimensionales/src/ejercicios/Ejercicios.java
@@ -0,0 +1,49 @@
+package ejercicios;
+
+import arrayUtility.ArrayUtility;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicios {
+
+    ArrayUtility a = new ArrayUtility();
+    Scanner s = new Scanner(System.in);
+
+    public void ej1(){
+        int[][] numeros = new int[3][3];
+        a.crearArrayInt(numeros,10);
+        a.recorrerArrayBidimensional(numeros);
+    }
+
+    public void ej2(){
+        int[][] numeros = new int[2][4];
+        a.rellenarArrayBidScanner(numeros);
+        a.recorrerArrayBidimensional(numeros);
+    }
+
+    public void ej3(){
+        int[][] numeros = new int[3][5];
+        int total = 0;
+        a.crearArrayInt(numeros,20);
+        a.recorrerArrayBidimensional(numeros);
+        System.out.println("El total de la suma del array es: "+a.sumaArrayBidimensional(numeros,total));
+    }
+
+    public void ej4(){
+        int[][] numeros = new int[3][5];
+        int numObjetivo;
+        a.crearArrayInt(numeros,100);
+        a.recorrerArrayBidimensional(numeros);
+        System.out.print("Introduce el numero a encontrar en la matriz: ");
+        numObjetivo = s.nextInt();
+        System.out.println(a.buscarEnArrayBid(numeros,numObjetivo));
+    }
+
+    public void ej5(){
+        double[][] numeros = new double[4][3];
+        a.crearArrayDouble(numeros,10);
+        a.recorrerArrayBidDouble(numeros);
+        a.mediaFila(numeros);
+    }
+}
