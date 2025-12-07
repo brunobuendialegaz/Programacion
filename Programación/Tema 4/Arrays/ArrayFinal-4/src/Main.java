4c59ee750ea9399b7c9af788a52e18aa9cce9b7e
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-12-07 10:38:20 +0100

    ej3 y 4 de complejos

---
+++ Programación/Tema 4/Arrays/ArrayFinal-4/src/Main.java
@@ -0,0 +1,13 @@
+import ej3.Ej3;
+import ej4.Ej4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ej3 e3 = new Ej3();
+        Ej4 e4 = new Ej4();
+        //e3.ej3();
+        e4.ej4();
+    }
+
+}
---
+++ Programación/Tema 4/Arrays/ArrayFinal-4/src/ej4/Ej4.java
@@ -0,0 +1,48 @@
+package ej4;
+
+import java.util.*;
+
+public class Ej4 {
+    Scanner s = new Scanner(System.in);
+    public void ej4(){
+
+        String[] bombo1 = new String[5];
+        String[] bombo2 = new String[5];
+
+        //Rellenamos los bombos
+        System.out.println("Introduce las selecciones del primer bombo: ");
+        for (int i = 0; i < bombo1.length; i++) {
+            System.out.print("Selección "+(i+1)+": ");
+            bombo1[i] = s.nextLine();
+        }
+        System.out.println("Introduce las selecciones del segundo bombo: ");
+        for (int i = 0; i < bombo2.length; i++) {
+            System.out.print("Selección "+(i+1)+": ");
+            bombo2[i] = s.nextLine();
+        }
+
+        //Recorremos los bombos
+        System.out.println("Primer bombo: ");
+        for (int i = 0; i < bombo1.length; i++) {
+            System.out.print(bombo1[i]+"\t");
+        }
+        System.out.println("\nSegundo bombo: ");
+        for (int i = 0; i < bombo2.length; i++) {
+            System.out.print(bombo2[i]+"\t");
+        }
+
+        //Los convierto a listas, tengo que investigar sobre estas, pero se pueden usar funcionalidades chulas
+        List<String> lista1 = Arrays.asList(bombo1);
+        List<String> lista2 = Arrays.asList(bombo2);
+
+        //Funcionalidades de collections, por ejemplo shuffle, esto hace el ejercicio gratis
+        Collections.shuffle(lista1);
+        Collections.shuffle(lista2);
+
+        System.out.println("\nEnfrentamientos de fase de grupos: ");
+        for (int i = 0; i < lista1.size(); i++) {
+            System.out.println(lista1.get(i)+" vs "+lista2.get(i));
+        }
+
+    }
+}
