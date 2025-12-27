bc9c6b4c2017484124e5de8e30649dc60657a47b
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2025-12-27 19:33:49 +0100

    nuevo algoritmo, comentarios actualizados

--- MPO/Proyecto Navidad/Cuadrado_Magico/src/agente/CuadradoMagico.java
+++ MPO/Proyecto Navidad/Cuadrado_Magico/src/agente/CuadradoMagico.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class CuadradoMagico {
 
+    /*
+    Este algoritmo es una "mejora" del anterior, saca mucho mas rapido el 5x5, en torno a unos 5 minutos, algunas veces
+    mucho menos, el problema es que es algo mas lento en los 3x3 y los 4x4, aunque no demasiado.
+    Para mi gusto lo peor es que he tenido que retocar todos los metodos de comprobación y finales,
+    haciendo para mi el codigo mucho menos legible.
+
+    https://github.com/brunobuendialegaz/Programacion/tree/main/MPO/Proyecto%20Navidad/Cuadrado_Magico
+
+     */
+
     //objeto cuadrado mágico, toda la lógica y ejecuciones van aqui
 
     private int tamanio;
@@ -68,23 +78,32 @@ public class CuadradoMagico {
         
         long contador =0L;
         do {
+            contador++;
             refrescarCuadrado();
             rellenarPool();
             
             rellenarDiagonalPrincipal();
             rellenarDiagonalSecundaria();
             
-            if (!comprobarDiagonalPrincipal()||!comprobarDiagonalSecundaria()) {
+            if (!comprobarDiagonalPrincipal(cuadradoMagico)||!comprobarDiagonalSecundaria(cuadradoMagico)) {
                 continue;
             }
+            int contadorSecundario = 0;
+            //tras darle una vuelta he añadido este bucle para aprovechar cuando estan bien las diagonales y hacer intentos
+            //por esto, he tenido que meter argumentos a todos los metodos de comprobación y de "ultimo"
+            //ya que para hacerlo tengo que hacer copias de la pool y del cuadrado magico. Por tanto necesito saber que comprobar
+            // en cada caso
+            while (contadorSecundario<1000&&!comprobarCuadrado(cuadradoMagico)){
+                cuadradoMagico = rellenarFilas(cuadradoMagico,pool);
+                contadorSecundario++;
+            }
+            contador += contadorSecundario;
 
-            rellenarFilas();
-            contador++;
-            if ((contador%1000000)==0) {
+            if ((contador%10000000)<=999) {
                 System.out.println("Llevamos "+(contador/1000000)+" millones de intentos... Seguimos...");
             }
             
-        } while (!comprobarCuadrado());
+        } while (!comprobarCuadrado(cuadradoMagico));
         mostrarCuadrado(contador);
     }
 
@@ -96,7 +115,7 @@ public class CuadradoMagico {
                 numAleatorio = (int)(Math.random()*pool.size());
                 cuadradoMagico[i][i] = pool.remove(numAleatorio);
             } else {
-                cuadradoMagico[i][i] = ultimoDiagonalPrincipal();
+                cuadradoMagico[i][i] = ultimoDiagonalPrincipal(pool,cuadradoMagico);
             }
         }    
     }
@@ -111,7 +130,7 @@ public class CuadradoMagico {
                     numAleatorio = (int)(Math.random()*pool.size());
                     cuadradoMagico[(cuadradoMagico.length-1)-i][i] = pool.remove(numAleatorio);
                 } else {
-                    cuadradoMagico[(cuadradoMagico.length-1)-i][i] = ultimoDiagonalSecundaria();
+                    cuadradoMagico[(cuadradoMagico.length-1)-i][i] = ultimoDiagonalSecundaria(pool, cuadradoMagico);
                 }
             }
 
@@ -120,33 +139,51 @@ public class CuadradoMagico {
 
     //Rellena las filas, lo uso despues de las 2 diagonales. Mete todas las filas salvo la ultima buscando el número necesario para que den la constante mágica.
     //En el caso de la última fila uso un comprobador de columnas para todas las columnas centrales (la primera y la última ya estan completas por las diagonales)
-    private void rellenarFilas(){
+    private int[][] rellenarFilas(int [][] matriz, ArrayList<Integer> pool){
+        int[][] matrizCopia = clonarMatriz(matriz);
+        ArrayList<Integer> poolCopia = new ArrayList<>(pool);
         int numAleatorio;
-        for (int i = 0; i < cuadradoMagico.length-1; i++) {
-            for (int j = 0; j < cuadradoMagico.length; j++) {
-                if (cuadradoMagico[i][j]==0) {
-                    if (j<cuadradoMagico.length-1) {
-                    numAleatorio = (int)(Math.random()*pool.size());
-                    cuadradoMagico[i][j] = pool.remove(numAleatorio); 
-                    }else{
-                        cuadradoMagico[i][j]=ultimoFila(i);
-                    } 
+        for (int i = 0; i < matriz.length-1; i++) {
+            for (int j = 0; j < matrizCopia.length; j++) {
+                if (matrizCopia[i][j]==0) {
+                    if (j<matrizCopia.length-1) {
+                        numAleatorio = (int)(Math.random()*poolCopia.size());
+                        matrizCopia[i][j] = poolCopia.remove(numAleatorio);
+                    }else {
+                        int numNecesario = ultimoFila(i, poolCopia, matrizCopia);
+                        if (numNecesario == -1) {
+                            return matriz;
+                        }
+                        matrizCopia[i][j] = numNecesario;
+                    }
                 }
-            }  
+            }
 
-            if(!comprobarFila(i)){
-                return;
+            if(!comprobarFila(i,matrizCopia)){
+                return matriz;
             }
         }
-        for (int i = 0; i < cuadradoMagico.length; i++) {
-            if(i!=0&&i!=cuadradoMagico.length-1){
-                int numNecesario = ultimoColumnas(i);
+        for (int i = 0; i < matriz.length; i++) {
+            if(i!=0&&i!=matrizCopia.length-1){
+                int numNecesario = ultimoColumnas(i,poolCopia,matrizCopia);
                 if(numNecesario==-1){
-                    return;
+                    return matriz;
                 }
-                cuadradoMagico[cuadradoMagico.length-1][i] = numNecesario;
+                matrizCopia[matrizCopia.length-1][i] = numNecesario;
             }
         }
+        return matrizCopia;
+    }
+
+    // clona la matriz para hacer sucesivas pruebas sin alterar las diagonales.
+    private int[][] clonarMatriz(int[][] matriz){
+        int[][] copia = new int[matriz.length][matriz.length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                copia[i][j] = matriz[i][j];
+            }
+        }
+        return copia;
     }
 
     //Muestra el cuadrado con el formato indicado en el ejercicio. Solo se ejecuta una vez es correcto.
@@ -163,10 +200,10 @@ public class CuadradoMagico {
     }
 
     //métodos de comprobación
-    private boolean comprobarFila(int i){
+    private boolean comprobarFila(int i, int[][] matriz){
         int num = 0;
-        for (int j = 0; j < cuadradoMagico.length; j++) {
-            num += cuadradoMagico[i][j];
+        for (int j = 0; j < matriz.length; j++) {
+            num += matriz[i][j];
         }
         if (num==constanteMagica) {
             return true;
@@ -175,10 +212,10 @@ public class CuadradoMagico {
         }
     }
 
-    private boolean comprobarColumna(int j){
+    private boolean comprobarColumna(int j, int[][] matriz){
         int num = 0;
-        for (int i = 0; i < cuadradoMagico.length; i++) {
-            num += cuadradoMagico[i][j];
+        for (int i = 0; i < matriz.length; i++) {
+            num += matriz[i][j];
         }
         if (num==constanteMagica) {
             return true;
@@ -187,10 +224,10 @@ public class CuadradoMagico {
         }
     }
 
-    private boolean comprobarDiagonalPrincipal(){
+    private boolean comprobarDiagonalPrincipal(int[][] matriz){
         int num = 0;
-        for (int i = 0; i < cuadradoMagico.length; i++) {
-            num += cuadradoMagico[i][i];
+        for (int i = 0; i < matriz.length; i++) {
+            num += matriz[i][i];
         }
         if (num==constanteMagica) {
             return true;
@@ -199,10 +236,10 @@ public class CuadradoMagico {
         }
     }
 
-    private boolean comprobarDiagonalSecundaria(){
+    private boolean comprobarDiagonalSecundaria(int[][] matriz){
         int num = 0;
-        for (int i = 0; i < cuadradoMagico.length; i++) {
-            num += cuadradoMagico[(cuadradoMagico.length-1)-i][i];
+        for (int i = 0; i < matriz.length; i++) {
+            num += matriz[(matriz.length-1)-i][i];
         }
         if (num==constanteMagica) {
             return true;
@@ -212,23 +249,24 @@ public class CuadradoMagico {
     }
 
     //método de comprobación general
-    private boolean comprobarCuadrado(){
-        for (int i = 0; i < cuadradoMagico.length; i++) {
-            if(!comprobarColumna(i)||!comprobarFila(i)){
+    private boolean comprobarCuadrado(int[][] matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            if(!comprobarColumna(i,matriz)||!comprobarFila(i,matriz)){
                 return false;
             }
         }
-        if(!comprobarDiagonalPrincipal()||!comprobarDiagonalSecundaria()){
+        if(!comprobarDiagonalPrincipal(matriz)||!comprobarDiagonalSecundaria(matriz)){
                 return false;
         }
         return true;
     }
 
     //buscadores de lus últimos números de filas, columna y diagonales.
-    private int ultimoFila(int i){
+    private int ultimoFila(int i, ArrayList<Integer> pool,int[][] matriz){
+        int[][] matrizCopia = clonarMatriz(matriz);
         int num = 0;
-        for (int j = 0; j < cuadradoMagico.length-1; j++) {
-            num += cuadradoMagico[i][j];
+        for (int j = 0; j < matrizCopia.length-1; j++) {
+            num += matrizCopia[i][j];
         }
         int index = pool.indexOf(constanteMagica-num);
         if (index!=-1) {
@@ -238,10 +276,11 @@ public class CuadradoMagico {
         return -1;
     }
 
-    private int ultimoColumnas(int f){
+    private int ultimoColumnas(int f, ArrayList<Integer> pool,int[][] matriz){
+        int[][] matrizCopia = clonarMatriz(matriz);
         int num = 0;
-        for (int i = 0; i < cuadradoMagico.length; i++) {
-            num += cuadradoMagico[i][f];
+        for (int i = 0; i < matrizCopia.length; i++) {
+            num += matrizCopia[i][f];
         }
         int index = pool.indexOf(constanteMagica-num);
         if (index!=-1){
@@ -251,10 +290,11 @@ public class CuadradoMagico {
         return -1;
     }
 
-        private int ultimoDiagonalPrincipal(){
+    private int ultimoDiagonalPrincipal(ArrayList<Integer> pool, int[][] matriz){
+        int[][] matrizCopia = clonarMatriz(matriz);
         int num = 0;
-        for (int i = 0; i < cuadradoMagico.length-1; i++) {
-            num += cuadradoMagico[i][i];
+        for (int i = 0; i < matrizCopia.length-1; i++) {
+            num += matrizCopia[i][i];
         }
         int index = pool.indexOf(constanteMagica-num);
         if (index!=-1) {
@@ -264,10 +304,11 @@ public class CuadradoMagico {
         return -1;
     }
     
-            private int ultimoDiagonalSecundaria(){
+    private int ultimoDiagonalSecundaria(ArrayList<Integer> pool, int[][] matriz){
+        int[][] matrizCopia = clonarMatriz(matriz);
         int num = 0;
-        for (int i = 0; i < cuadradoMagico.length-1; i++) {
-            num += cuadradoMagico[(cuadradoMagico.length-1)-i][i];
+        for (int i = 0; i < matrizCopia.length-1; i++) {
+            num += matrizCopia[(matrizCopia.length-1)-i][i];
         }
         int index = pool.indexOf(constanteMagica-num);
         if (index!=-1) {
