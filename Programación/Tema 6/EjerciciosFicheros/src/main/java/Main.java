54d727639f2270d1847d09c40019838d08f7c449
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-02-16 18:14:52 +0100

    comentarios

--- Programación/Tema 6/EjerciciosFicheros/src/main/java/Main.java
+++ Programación/Tema 6/EjerciciosFicheros/src/main/java/Main.java
@@ -13,13 +13,13 @@ public class Main {
         e.ej2();
         e.ej3();
         e.ej4();
-        e.ej5();*/
+        e.ej5();
         List<Alumno> alumnos = new ArrayList<>();
         alumnos.add(new Alumno("Bruno",30,9.5));
         alumnos.add(new Alumno("Daniel",34,9.9));
         alumnos.add(new Alumno("Dano",26,9.2));
         e.ej6(alumnos);
-        e.ej1();
+        e.ej1();*/
 
 
 
