56fbfac127eea8f66ac3989c1b0c67821e9ad1cb
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-03-31 13:54:16 +0200

    views and controllers

--- MPO/proyectoSemanaSanta/src/main/java/org/example/proyectosemanasanta/controller/UserController.java
+++ MPO/proyectoSemanaSanta/src/main/java/org/example/proyectosemanasanta/controller/UserController.java
@@ -1,11 +1,20 @@
 package org.example.proyectosemanasanta.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import lombok.Data;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Data
 public class UserController implements Initializable {
+
+    @FXML
+    private Button CheckInOutButton;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
