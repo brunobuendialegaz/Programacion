2d2e423f951003934456f526f5f30d597e469eab
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-03-31 20:56:03 +0200

    controller view general fichajes

---
+++ MPO/proyectoSemanaSanta/src/main/java/org/example/proyectosemanasanta/controller/AdminCheckInOutController.java
@@ -0,0 +1,4 @@
+package org.example.proyectosemanasanta.controller;
+
+public class AdminCheckInOutController {
+}
--- MPO/proyectoSemanaSanta/src/main/java/org/example/proyectosemanasanta/model/Record.java
+++ MPO/proyectoSemanaSanta/src/main/java/org/example/proyectosemanasanta/model/Record.java
@@ -2,5 +2,9 @@ package org.example.proyectosemanasanta.model;
 
 public class Record {
 
+    private String time, type;
+
+    private User user;
+
 
 }
